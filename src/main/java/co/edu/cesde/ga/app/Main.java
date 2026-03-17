9ed2c3734d1e891cc5becb9d72e924f695b3ea82
Gary1913 <yuleith07@gmail.com>
2026-03-17 10:11:45 -0500

    se convierte Person en clase abstracta, se corrigen los ID de estudent y teacher, se crea la interfaz StudentRepository

--- src/main/java/co/edu/cesde/ga/app/Main.java
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -1,70 +1,16 @@
 package co.edu.cesde.ga.app;
-import co.edu.cesde.ga.model.Person;
+
 import co.edu.cesde.ga.model.Student;
 import co.edu.cesde.ga.model.Teacher;
 
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person();
-
-        person.setUserId(1);
-        person.setCode("12345");
-        person.setDocumentNumber("112233");
-        person.setFirstName("Luis");
-        person.setLastName("Suarez");
-        person.setStatus("ACTIVE");
-
-        Teacher teacher = new Teacher();
-
-        teacher.setUserId(2);
-        teacher.setCode("12345");
-        teacher.setDocumentNumber("112233");
-        teacher.setFirstName("Miguel");
-        teacher.setLastName("Ocampo");
-        teacher.setStatus("ACTIVE");
-
-        Student student = new Student();
-        student.setUserId(3);
-        student.setCode("4567");
-        student.setDocumentNumber("0001");
-        student.setFirstName("Nancy");
-        student.setLastName("Vergara");
-        student.setStatus("ACTIVE");
-
-
-        System.out.println(person);
-        System.out.println( "User id : " + person.getUserId());
-        System.out.println( "Document number " + person.getDocumentNumber());
-        System.out.println( "Code : " + person.getCode());
-        System.out.println( "First name : " + person.getFirstName());
-        System.out.println( "Last name : " + person.getLastName());
-        System.out.println("Status : " + person.getStatus());
-
-
-
-        System.out.println(teacher);
-        System.out.println( "User id : " + teacher.getUserId());
-        System.out.println( "Code : " + teacher.getCode());
-        System.out.println("Document number : " + teacher.getDocumentNumber());
-        System.out.println( "First name : " + teacher.getFirstName());
-        System.out.println( "Last name : " + teacher.getLastName());
-        System.out.println("Status : " + teacher.getStatus());
-
-        System.out.println(student);
-        System.out.println("user id : " + student.getUserId());
-        System.out.println("code : " + student.getCode());
-        System.out.println("Document number : " + student.getDocumentNumber());
-        System.out.println("first name : " + student.getFirstName());
-        System.out.println("last name : " + student.getLastName());
-        System.out.println("status : " + student.getStatus());
-        System.out.println(Student.getBirthDate());
-
-
-
 
 
     }
-
-
 }
+
+
+
+
--- src/main/java/co/edu/cesde/ga/model/Student.java
+++ src/main/java/co/edu/cesde/ga/model/Student.java
@@ -2,21 +2,44 @@ package co.edu.cesde.ga.model;
 
 public class Student  extends  Person
 {
-   private static String birthDate;
+    private Long studentId;
+   private  String birthDate;
+
     public Student(){
+        super();
 
     }
-    public Student(long userId, String code, String documentNumber, String firstName, String lastName, String status, String birthday) {
-        super(userId, code, documentNumber, firstName, lastName, status);
+    public Student( long studentId, long userId, String documentType, String documentNumber, String firstName, String lastName, String status, String birthday) {
+        super(userId, documentType, documentNumber, firstName, lastName, status);
         this.birthDate = birthDate;
+        this.studentId = studentId;
+    }
+    public Long getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
     }
 
-    public static String getBirthDate() {
+    public  String getBirthDate() {
         return birthDate;
     }
 
     public void setBirthDate(String birthDate) {
         this.birthDate = birthDate;
+    }
+    @Override
+        public String toString() {
+            return "Student{ " +
+                    "studentId= " + getStudentId() + '\n' +
+                    ", userId= " + getUserId() +
+                    ", documentType= " + getDocumentType() + '\n' +
+                    ", documentNumber= " + getDocumentNumber() + '\n' +
+                    ", firstName= " + getFirstName() + '\n' +
+                    ", lastName= " + getLastName() + '\n' +
+                    ", birthDate= " + getBirthDate() + '\n' +
+                    ", status= " + getStatus() + '\n' + " } ";
+
     }
 }
 
